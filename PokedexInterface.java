2ec31cf1f6290686bae2c22ed4bedc3b4914d452
blcarpenter <60524591+blcarpenter@users.noreply.github.com>
2021-03-22 22:47:09 -0500

    Update PokedexInterface.java

--- PokedexInterface.java
+++ PokedexInterface.java
@@ -2,6 +2,10 @@ public interface PokedexInterface {
 
   public Pokemon getPokemon(Backend backend, int id);
   public boolean addPokemon(Backend backend, Pokemon pokemon);
-  public String displayRange(Backend backend, int start, int end);
+  public static String displayRange(Backend backend, int start, int end) {
+
+      return backend.displayAllInRange(start, end).toString();
+
+  };
 
 }
